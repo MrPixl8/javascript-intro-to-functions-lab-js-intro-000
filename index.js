242307fe35c2d25ae7499a5d0bf64c6e897be0f9
Jake Lovelace <jaglovelace@gmail.com>
2018-05-17 15:03:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var lowercase = "hello!"
 lowercase.toLowerCase() === lowercase
 
 function shout(string) {
-  console.log (shout)
+  console.log(shout)
   return string.toUpperCase()
 }
 
@@ -10,16 +10,16 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string){
+function logShout(string) {
   console.log("HELLO")
 }
 
-function logWhisper(string){
-  console.log ("hello")
+function logWhisper(string) {
+  console.log("hello")
 }
 
 function sayHiToGrandma(string) {
-   if(string === lowercase){
-   return "I can\'t hear you!";
-   }
+  if (string.lowerCase() === string) {
+    return "I can't hear you!";
+  }
 }
