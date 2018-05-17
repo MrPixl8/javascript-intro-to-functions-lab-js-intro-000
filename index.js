36aa6706ae7810bad4967ff2496a083f6f166f76
Jake Lovelace <jaglovelace@gmail.com>
2018-05-17 15:00:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string){
   console.log ("hello")
 }
 
-function sayHiToGrandma (string) {
+function sayHiToGrandma(string) {
    if(string === lowercase){
    return "I can\'t hear you!";
    }
